ca5c3beb9ea67192c3780a7daf782257821d8d63
hyejun <hyejun0608@naver.com>
2021-08-18 20:16:14 +0900

    [ADD] main component UI

---
+++ src/components/main/Main.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import * as S from './style';
+import Header from '../header';
+
+const Main = () => {
+  return (
+    <S.Main>
+      <Header />
+    </S.Main>
+  );
+};
+
+export default Main;
---
+++ src/components/main/style/index.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+import { color } from '../../../style';
+
+export const Main = styled.div`
+  width: 100%;
+  min-width: 1440px;
+  height: 100vh;
+`;
