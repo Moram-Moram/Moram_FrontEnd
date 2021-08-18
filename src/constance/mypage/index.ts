49c924985a102041d57ee6ce5aaf30edb7e9b27b
hyejun <hyejun0608@naver.com>
2021-08-18 20:07:47 +0900

    [ADD] mypage constance

---
+++ src/constance/mypage/index.ts
@@ -0,0 +1,5 @@
+export const MYPOST = '내가 작성한 게시글';
+export const MODIFYINFO = '정보수정';
+export const CURRENTPASSWORD = '현재 비밀번호';
+export const MODIFYPASSWORD = '새로운 비밀번호';
+export const STARTEXPLAIN = '처음 시작 화면을 흰 화면으로 시작하기';
