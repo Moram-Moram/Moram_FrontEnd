7a3a6074cd9616d81cf127c70e49e10721dd808b
hyejun <hyejun0608@naver.com>
2021-08-18 20:09:54 +0900

    [ADD] login components UI

---
+++ src/components/login/Login.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import * as S from './style';
+import Header from '../header';
+import { LOGIN, ID, PASSWORD, START, ISFIRST, SIGNUP } from '../../constance/login';
+
+const Login = () => {
+  return (
+    <S.Login>
+      <Header />
+      <S.LoginBox>
+        <S.LoginTitle>{LOGIN}</S.LoginTitle>
+        <S.InputLine>
+          <S.InputTitle>{ID}</S.InputTitle>
+          <S.Input />
+          <S.InputTitle>{PASSWORD}</S.InputTitle>
+          <S.Input />
+        </S.InputLine>
+        <S.StartBtn>{START}</S.StartBtn>
+        <S.IsSignup>{ISFIRST}</S.IsSignup>
+        <S.Signup to='/signup'>{SIGNUP}</S.Signup>
+      </S.LoginBox>
+    </S.Login>
+  );
+};
+
+export default Login;
---
+++ src/components/login/style/index.ts
@@ -0,0 +1,75 @@
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import { color } from '../../../style';
+
+export const Login = styled.div`
+  width: 100%;
+  min-width: 1440px;
+  height: 100vh;
+  background-color: #fafafa;
+`;
+
+export const LoginBox = styled.div`
+  width: 360px;
+  height: 518px;
+  background-color: white;
+  margin: 0 auto;
+  margin-top: 9.5%;
+  border: 1px solid ${color.border};
+  border-radius: 20px;
+  box-shadow: 1px 1px 5px rgba(0, 0, 0, 0.16);
+`;
+
+export const LoginTitle = styled.p`
+  font-weight: bold;
+  font-size: 30px;
+  color: ${color.main};
+  text-align: center;
+  margin: 60px 0px 40px 0px;
+`;
+
+export const InputLine = styled.div`
+  width: 275px;
+  margin: 0 auto;
+`;
+
+export const InputTitle = styled.p`
+  font-size: 15px;
+  margin-bottom: 10px;
+`;
+
+export const Input = styled.input`
+  width: 100%;
+  height: 45px;
+  border: 1px solid ${color.inputBorder};
+  border-radius: 10px;
+  margin-bottom: 15px;
+`;
+
+export const StartBtn = styled.div`
+  width: 275px;
+  height: 45px;
+  background-color: #e8e8e8;
+  border-radius: 10px;
+  color: ${color.explain};
+  text-align: center;
+  margin: 20px auto 25px auto;
+  box-sizing: border-box;
+  padding: 14px;
+`;
+
+export const IsSignup = styled.p`
+  font-size: 14px;
+  color: ${color.explain};
+  text-align: center;
+  margin-bottom: 6px;
+`;
+
+export const Signup = styled(Link)`
+  width: 100%;
+  font-size: 14px;
+  font-weight: bold;
+  color: ${color.explain};
+  display: flex;
+  justify-content: center;
+`;
