a20a26d3e311283127fc1f5437f551fdbdd72ed5
hyejun <hyejun0608@naver.com>
2021-08-18 20:16:49 +0900

    [ADD] export signup, login, main, mypage, post

---
+++ src/components/index.ts
@@ -0,0 +1,5 @@
+export { default as Signup } from './signup';
+export { default as Login } from './login';
+export { default as Main } from './main';
+export { default as Mypage } from './mypage';
+export { default as Post } from './post';
