919538b0310d97fef934e44f0d6cbd065251943a
hyejun <hyejun0608@naver.com>
2021-08-18 20:08:11 +0900

    [ADD] login constance

---
+++ src/constance/login/index.ts
@@ -0,0 +1,6 @@
+export const LOGIN = '로그인';
+export const ID = '아이디';
+export const PASSWORD = '비밀번호';
+export const START = '들어가기';
+export const ISFIRST = '모람모람이 처음이신가요?';
+export const SIGNUP = '회원가입하기';
