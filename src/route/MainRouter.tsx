87f6868c060e66871ab6afa611f9c8c6cfedcd89
hyejun <hyejun0608@naver.com>
2021-08-18 20:15:55 +0900

    [ADD] signup, login, main, mypage, post-view 추가

--- src/route/MainRouter.tsx
+++ src/route/MainRouter.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { Signup } from '../components';
+import { Signup, Login, Main, Mypage, Post } from '../components';
 
 const MainRouter = () => {
   return (
     <Router>
       <Switch>
         <Route exact path='/signup' component={Signup} />
+        <Route exact path='/login' component={Login} />
+        <Route exact path='/' component={Main} />
+        <Route exact path='/mypage' component={Mypage} />
+        <Route exact path='/post/view' component={Post} />
       </Switch>
     </Router>
   );
