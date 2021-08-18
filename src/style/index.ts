e4d5ea97e1a1b34090cd0e41d327065e7e5b0809
hyejun <hyejun0608@naver.com>
2021-08-18 20:06:45 +0900

    [FIX] 공통으로 사용되는 색상 모음

--- src/style/index.ts
+++ src/style/index.ts
@@ -1,2 +1,11 @@
 import GlobalStyle from './GlobalStyle';
 export default GlobalStyle;
+
+export const color = {
+  main: '#02AAB2',
+  light: '#7FD3D1',
+  explain: '#6C6C6C',
+  border: '#D8D8D8',
+  inputBorder: '#707070',
+  btnBackground: '#E8E8E8',
+};
