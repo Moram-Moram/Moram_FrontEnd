8b11f111de8c3fc69883431a6e3464c757d85f62
hyejun <hyejun0608@naver.com>
2021-09-17 23:55:41 +0900

    [ADD] post img

---
+++ src/assets/post/index.ts
@@ -0,0 +1,5 @@
+export { default as backBtn } from './back_btn.svg';
+export { default as emptyHeart } from './empty_heart.svg';
+export { default as fillHeart } from './fill_heart.svg';
+export { default as report } from './report.svg';
+export { default as CommentProfile } from './comment_profile.svg';
