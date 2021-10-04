a04d885d1a8a050588163de8ac22497ecc3784e2
hyejun <hyejun0608@naver.com>
2021-10-04 23:04:11 +0900

    [ADD] signup constance

--- src/constance/signup/index.ts
+++ src/constance/signup/index.ts
@@ -3,6 +3,19 @@ export const SINGUPINPUT = [
   { id: 'id', content: '아이디', duplicateCheck: true },
   { id: 'password', content: '비밀번호', duplicateCheck: false },
 ];
-
 export const WHITESTART = '처음 시작 화면을 흰 화면으로 시작';
 export const START = '시작하기';
+
+export type signupType = {
+  id: string;
+  password: string;
+  nickname: string;
+  whiteCheck: boolean;
+};
+
+export interface signupInterface {
+  id: string;
+  password: string;
+  nickname: string;
+  whiteCheck: boolean;
+}
