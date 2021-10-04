6158b03a3017b4ffc036f847ca25eaa190770f0f
hyejun <hyejun0608@naver.com>
2021-10-04 23:03:15 +0900

    [FIX] index.ts

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { RecoilRoot } from 'recoil';
 
-ReactDOM.render(
-  <RecoilRoot>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </RecoilRoot>,
-  document.getElementById('root'),
-);
+ReactDOM.render(<App />, document.getElementById('root'));
