9e87a1c81913e772b9948ab7438e42829c53f0fd
hyejun <hyejun0608@naver.com>
2021-10-04 22:57:22 +0900

    [FIX] signup component로 연결한 것 container로 수정

--- src/route/MainRouter.tsx
+++ src/route/MainRouter.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Signup, Login, Main, Mypage, Post } from '../components';
+import { SignupContainer } from '../container';
 
 const MainRouter = () => {
   return (
     <Router>
       <Switch>
-        <Route exact path='/signup' component={Signup} />
+        <Route exact path='/signup' component={SignupContainer} />
         <Route exact path='/login' component={Login} />
         <Route exact path='/' component={Main} />
         <Route exact path='/mypage' component={Mypage} />
