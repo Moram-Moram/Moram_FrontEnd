82647fe0fc9acb97832dcb13300d850a14eca4cb
hyejun <hyejun0608@naver.com>
2021-10-04 23:01:51 +0900

    [ADD] default api

---
+++ src/util/api/default/index.ts
@@ -0,0 +1,30 @@
+import axios from 'axios';
+
+export const getRequest = () => {
+  const request = axios.create({
+    timeout: 10000,
+    baseURL: 'https://13.58.212.162',
+    withCredentials: true,
+    headers: {
+      withCredentials: true,
+    },
+  });
+  return request;
+};
+
+export const getRequestWithAccessToken = (
+  token: string,
+  type: 'json' | 'blob' | 'text' = 'json',
+) => {
+  const request = axios.create({
+    timeout: 10000,
+    baseURL: 'https://13.58.212.162',
+    headers: {
+      Authorization: `Bearer ${token}`,
+      withCredentials: true,
+    },
+    responseType: type,
+    withCredentials: true,
+  });
+  return request;
+};
