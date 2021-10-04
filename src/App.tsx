e7e0378330c34299adc5834f4d35756e5b02eae3
hyejun <hyejun0608@naver.com>
2021-10-04 22:59:06 +0900

    [FIX] Provider 추가를 안해서 추가를 해줬습니다

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import store from './modules/redux/store';
 import MainRouter from './route';
 import GlobalStyle from './style';
 
 const App = () => {
   return (
-    <>
+    <Provider store={store}>
       <GlobalStyle />
       <MainRouter />
-    </>
+    </Provider>
   );
 };
 
