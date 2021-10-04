2b6af114b2d3f92289f856cd8bdb690b77f813d2
hyejun <hyejun0608@naver.com>
2021-10-04 22:58:40 +0900

    [FIX] signup container state값도 props로 넘겨움

--- src/container/signup/SignupContainer.tsx
+++ src/container/signup/SignupContainer.tsx
@@ -3,8 +3,8 @@ import Signup from '../../components/signup';
 import useSignup from '../../util/hooks/signup';
 
 const SignupContainer = () => {
-  const { setState } = useSignup();
-  return <Signup {...setState} />;
+  const { state, setState } = useSignup();
+  return <Signup {...state} {...setState} />;
 };
 
 export default SignupContainer;
