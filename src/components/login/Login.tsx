a60cd3b6e228aaa32c5f268ec968f8130f7556e5
hyejun <hyejun0608@naver.com>
2021-10-20 20:20:31 +0900

    [ADD] 서버 통신

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -1,9 +1,39 @@
-import React from 'react';
+import React, { FC, useEffect, useState } from 'react';
 import * as S from './style';
 import Header from '../header';
-import { LOGIN, ID, PASSWORD, START, ISFIRST, SIGNUP } from '../../constance/login';
+import { LOGIN } from '../../modules/redux/action/auth/interface';
+import { ID, PASSWORD, START, ISFIRST, SIGNUP } from '../../constance/login';
+import { useDispatch } from 'react-redux';
 
-const Login = () => {
+interface Props {
+  id: string;
+  password: string;
+  setId: (payload: string) => void;
+  setPassword: (payload: string) => void;
+}
+
+const Login: FC<Props> = props => {
+  const { id, password, setId, setPassword } = props;
+  const [start, setStart] = useState(false);
+  const dispatch = useDispatch();
+
+  const idChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setId(event.currentTarget.value);
+  };
+
+  const passwordChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.currentTarget.value);
+  };
+
+  useEffect(() => {
+    if (id && password) setStart(true);
+    else setStart(false);
+  }, [id, password]);
+
+  const startBtnClickHandler = () => {
+    if (start) dispatch({ type: LOGIN });
+  };
+
   return (
     <S.Login>
       <Header />
@@ -11,11 +41,11 @@ const Login = () => {
         <S.LoginTitle>{LOGIN}</S.LoginTitle>
         <S.InputLine>
           <S.InputTitle>{ID}</S.InputTitle>
-          <S.Input />
+          <S.Input onChange={idChangeHandler} />
           <S.InputTitle>{PASSWORD}</S.InputTitle>
-          <S.Input type='password' />
+          <S.Input type='password' onChange={passwordChangeHandler} />
         </S.InputLine>
-        <S.StartBtn>
+        <S.StartBtn isStart={start} onClick={startBtnClickHandler}>
           <p>{START}</p>
         </S.StartBtn>
         <S.IsSignup>{ISFIRST}</S.IsSignup>
