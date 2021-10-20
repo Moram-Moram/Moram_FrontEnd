7d21ea45d6cbb1070b7e25c6fafff176097e3767
hyejun <hyejun0608@naver.com>
2021-10-20 20:22:50 +0900

    [ADD] main router에 auth container 추가

--- src/route/MainRouter.tsx
+++ src/route/MainRouter.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { Signup, Login, Main, Mypage, Post } from '../components';
-import { SignupContainer } from '../container';
+import { Main, Mypage, Post } from '../components';
+import { SignupContainer, AuthContainer } from '../container';
 
 const MainRouter = () => {
   return (
     <Router>
       <Switch>
         <Route exact path='/signup' component={SignupContainer} />
-        <Route exact path='/login' component={Login} />
+        <Route exact path='/login' component={AuthContainer} />
         <Route exact path='/' component={Main} />
         <Route exact path='/mypage' component={Mypage} />
         <Route exact path='/post/view' component={Post} />
